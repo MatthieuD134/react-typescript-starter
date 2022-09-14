1ae396b2f03c6171b3959a52b5585e8fe2ef9a18
Matthieu Daulhiac <matthieu@blanknetwork.com>
2022-09-14 10:21:40 +0800

    add tilt effect to card

--- src/components/common/Card/SquareImageCard.stories.tsx
+++ src/components/common/Card/SquareImageCard.stories.tsx
@@ -56,3 +56,17 @@ LongChildren.args = {
     'https://static.remove.bg/remove-bg-web/37843dee2531e43723b012aa78be4b91cc211fef/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
   imageAlt: 'just an image',
 };
+
+export const TiltEffect = Template.bind({});
+TiltEffect.args = {
+  children: (
+    <div>
+      <span>Card Title</span>
+      <p>Card description here...</p>
+    </div>
+  ),
+  image:
+    'https://static.remove.bg/remove-bg-web/37843dee2531e43723b012aa78be4b91cc211fef/assets/start-1abfb4fe2980eabfbbaaa4365a0692539f7cd2725f324f904565a9a744f8e214.jpg',
+  imageAlt: 'just an image',
+  tiltHoverEffect: true,
+};
--- src/components/common/Card/SquareImageCard.tsx
+++ src/components/common/Card/SquareImageCard.tsx
@@ -7,6 +7,7 @@ interface SquareImageCardProps {
   imageAlt: string;
   onClick?: React.MouseEventHandler<HTMLDivElement>;
   disableTabNav?: boolean;
+  tiltHoverEffect?: boolean;
 }
 
 const SquareImageCard = ({
@@ -15,17 +16,49 @@ const SquareImageCard = ({
   imageAlt,
   onClick,
   disableTabNav = false,
+  tiltHoverEffect = false,
 }: SquareImageCardProps) => {
   const [imgLoaded, setImageLoaded] = useState(false);
   return (
-    <div className={`${styles.card}`} tabIndex={disableTabNav ? -1 : 0} onClick={onClick}>
-      <img
-        className={`${styles.cardImg} ${imgLoaded ? styles.imgLoaded : styles.imgLoading}`}
-        src={image}
-        alt={imageAlt}
-        onLoad={() => setImageLoaded(true)}
-      />
-      {children && <span className={styles.cardSpan}>{children}</span>}
+    <div className={styles.cardContainer}>
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      {tiltHoverEffect && <div className={styles.tiltBox} />}
+      <div className={`${styles.card}`} tabIndex={disableTabNav ? -1 : 0} onClick={onClick}>
+        <img
+          className={`${styles.cardImg} ${imgLoaded ? styles.imgLoaded : styles.imgLoading}`}
+          src={image}
+          alt={imageAlt}
+          onLoad={() => setImageLoaded(true)}
+        />
+        {children && <span className={styles.cardSpan}>{children}</span>}
+      </div>
     </div>
   );
 };
